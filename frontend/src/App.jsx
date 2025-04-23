43ecedc5a7e8d0acedff99a67f4779e5d66e753d
Alec Faustino <alecfaustino@outlook.com>
2025-04-23 09:45:49 -0600

    add topicList typography component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import PhotoList from "./components/PhotoList";
 import "./App.scss";
+import TopicList from "./components/TopicList";
 
 const App = () => {
   return (
     <div className="App">
+      <TopicList />
       <PhotoList />
     </div>
   );
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,15 +1,10 @@
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: 1,
-  slug: "topic-1",
-  label: "Nature",
-};
-
-const TopicListItem = () => {
+const TopicListItem = ({ topic }) => {
+  console.log("@@@@123@@@", topic);
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
+      <span>{topic.title}</span>
     </div>
   );
 };
